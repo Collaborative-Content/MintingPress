a1c7b3e2b566c445c35ead1ca328a44a52b708d5
rsundar <rohan.s.1987@gmail.com>
2022-01-24 01:31:40 +0530

    created negative test for the calculatePurchaseReturn function

--- test/ContentFactory.js
+++ test/ContentFactory.js
@@ -4,8 +4,8 @@ const { ethers } = require("hardhat");
 describe("ContentFactory Contract", () => {
     let ContentFactory, contract;
     let totalSupply = 100000;
-    let ownerStake = 5000;
-    let startingPrice = 2;
+    let ownerStake = 50000;
+    let startingPrice = 20000;
     let tokenName = "test";
     let tokenSymbol = "tst";
     let content = "some test content";
@@ -34,6 +34,12 @@ describe("ContentFactory Contract", () => {
         expect(balances)
             .to
             .equals(ownerStake);
+    });
+
+    it("calculatePurchaseReturn function, when price < startingPrice", async () => {
+      let price = 15000;
+      await expect(contract.calculatePurchaseReturn(price)).to.be
+      .revertedWith('Below the minimum value for the pull request');
     });
 });
 
