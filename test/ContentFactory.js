00db6782e3f1fbf119d4407fa78cb975f45df4c6
rsundar <rohan.s.1987@gmail.com>
2022-01-23 22:57:36 +0530

    finally got the tests to pass!

--- test/ContentFactory.js
+++ test/ContentFactory.js
@@ -2,19 +2,19 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("ContentFactory Contract", () => {
-    let ContentFactory, contentFactory;
-    let totalSupply = 100000
-    let ownerStake = 5000
-    let startingPrice = 2
-    let tokenName = "test"
-    let tokenSymbol = "tst"
-    let content = "some test content"
-    let owner
+    let ContentFactory, contract;
+    let totalSupply = 100000;
+    let ownerStake = 5000;
+    let startingPrice = 2;
+    let tokenName = "test";
+    let tokenSymbol = "tst";
+    let content = "some test content";
+    let owner;
 
     beforeEach(async () => {
-        owner = await ethers.getSigners();
+        [owner] = await ethers.getSigners();
         ContentFactory = await ethers.getContractFactory("ContentFactory");
-        contentFactory = await ContentFactory.deploy(totalSupply, ownerStake, startingPrice, tokenName, tokenSymbol, content);
+        contract = await ContentFactory.deploy(totalSupply, ownerStake, startingPrice, tokenName, tokenSymbol, content);
     });
 
     //it("emit greeting event when greet function is called", async () => {
@@ -25,10 +25,10 @@ describe("ContentFactory Contract", () => {
     //});
 
     it("Test if owner's stake is set to the total outstanding mapping ", async () => {
-        let balances = await contentFactory.getBalances(owner.address);
+        let balances = await contract.getBalances(owner.address);
         expect(balances)
             .to
-            .equal(ownerStake);
+            .equals(ownerStake);
     });
 });
 
