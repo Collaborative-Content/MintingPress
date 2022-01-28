6f7575b1ed9d17cf844cd293b50c04d5f4b544da
mardet987 <marci.detwiller@gmail.com>
2022-01-27 21:03:53 -0600

    assignVoteCredits

--- contracts/Content.sol
+++ contracts/Content.sol
@@ -10,16 +10,19 @@ import "./libraries/utils.sol";
 import "./Settings.sol";
 import "./AdminProxy.sol";
 
+library BondingCurveLib {
+
+
+
+}
+
+library PRLib {
+
+}
+
 contract Content is ERC1155, Ownable, IERC1155Receiver{
-    AdminProxy immutable adminProxy;
-    Settings immutable settings;
-    address public contentContract;
-
-    // content mapping
-    uint public contentTokenID = 1;
-    mapping(uint => bytes) public contentData;
-
-    bool public contentComplete;
+    //using BondingCurveLib for BondingCurveLib.BondingCurve;
+    //using PRLib for PRLib.PR;
 
     struct BondingCurve {
         bytes tokenSymbol;
@@ -28,10 +31,6 @@ contract Content is ERC1155, Ownable, IERC1155Receiver{
         uint minPRPrice;
         uint ownerStake;
     }
-
-    mapping( uint => BondingCurve ) bondingCurveParams;
-    mapping( uint => mapping( address => uint )) outstandingFungibleBalance;
-    mapping( uint => uint ) tokenReserveCounter;
 
     function setBondingCurveParams(bytes memory _tokenSymbol,
     uint _tokenID, 
@@ -45,23 +44,32 @@ contract Content is ERC1155, Ownable, IERC1155Receiver{
         bondingCurveParams[_tokenID].totalSupply = _totalSupply;
         tokenReserveCounter[_tokenID] += _ownerStake;
     }
-    
-    /*
-    struct voteTally {
-        uint positiveVotes;
-        uint negativeVotes;
-    }
-    */
 
     struct PR {
         bytes content;
         uint blockTimestamp;
-        // voteTally PRVoteTally;
         uint positiveVotes;
         uint negativeVotes;
         uint totalVotes;
         uint PRPrice;
     }
+
+    AdminProxy immutable adminProxy;
+    Settings immutable settings;
+    address public contentContract;
+
+    // content mapping
+    uint public contentTokenID = 1;
+    mapping(uint => bytes) public contentData;
+    mapping(uint => bool) public contentComplete;
+    mapping(uint => address[]) public contentAuthors;
+
+    mapping( uint => BondingCurve ) bondingCurveParams;
+    // TODO can we remove outstandingFungibleBalance and replace with balanceof() from ERC1155?
+    mapping( uint => mapping( address => uint )) outstandingFungibleBalance; 
+    // TODO can we put tokenReserveCounter inside the BondingCurve struct??
+    mapping( uint => uint ) tokenReserveCounter;
+
 
     mapping(uint => mapping(address => PR)) public PRs;
     mapping(uint => mapping(address => bool)) public PRexists;
@@ -69,6 +77,9 @@ contract Content is ERC1155, Ownable, IERC1155Receiver{
     mapping(uint => address[]) public topPRAuthors;
 
     mapping(uint => mapping(address => uint)) public voteCredits;
+
+    // TODO implement completion vote
+    mapping(uint => mapping(address => bool)) votesToComplete;
 
     event Voted(address voter, address PRowner, uint voteCredits, bool positive, uint tokenID);
     event PRApproved(uint tokenID, address PRwinner);
@@ -137,21 +148,17 @@ contract Content is ERC1155, Ownable, IERC1155Receiver{
     // @params data - story, symbol - fungible token 
     function mint(bytes memory data, uint totalSupply, uint minPRPrice, uint ownerStake, bytes memory tokenSymbol) external payable {
         // PUT IN BONDING CURVE METHOD
+        // TODO other checks on bonding curve based on additional settings
         require(minPRPrice >= settings.MinimumPRPrice(), "Min PR Price is too low");
         require(msg.value >= settings.MinimumInitialPrice(), "Not enough ETH to mint content");
         require(totalSupply >= settings.MinimumInitialSupply(), "Total Supply too low");
         require(ownerStake <= totalSupply, "Owner stake must be less than supply");
         
         setBondingCurveParams(tokenSymbol, contentTokenID - 1, ownerStake,  minPRPrice, totalSupply);
-
-        // TODO other checks on bonding curve based on additional settings
-
-
         super._mint(contentContract, contentTokenID, 1, data); // non fungible
         _mintOwnership(msg.sender, contentTokenID-1, bondingCurveParams[contentTokenID-1].ownerStake, bondingCurveParams[contentTokenID - 1].tokenSymbol);   // fungible
         emit NewContentMinted(contentTokenID, msg.sender);
         contentTokenID += settings.ReserveTokenSpaces();  // increment to make space for new content
-
     }
 
     function _mintOwnership(address to, uint id, uint amount, bytes memory data) internal {
@@ -179,9 +186,19 @@ contract Content is ERC1155, Ownable, IERC1155Receiver{
         emit NewPR(msg.sender, tokenID, msg.value, amount);
     }
 
+    // TODO function for admin to assign vote credits at start of voting; Otherwise there is a bug 
+    function _assignVoteCredits() external onlyOwner {
+        for (uint tokenId = 0; tokenId < contentTokenID; tokenId=tokenId+2) {
+            for (uint i = 0; i < contentAuthors[tokenId].length; i++) {
+                address author = contentAuthors[tokenId][i];
+                voteCredits[tokenId][author] == outstandingFungibleBalance[tokenId][author] ** 2;
+            }
+        }
+    }
+
     function vote(address _PRowner, uint _numVotes, bool positive, uint tokenId) external onlyAuthor(tokenId) {
         require(adminProxy.votingOpen(), "Voting is currently closed");
-        voteCredits[tokenId][msg.sender] == outstandingFungibleBalance[tokenId][msg.sender] ** 2;
+        //voteCredits[tokenId][msg.sender] == outstandingFungibleBalance[tokenId][msg.sender] ** 2;
         require((_numVotes <= voteCredits[tokenId][msg.sender]), "Not enough vote credits");
         PR storage votingPR = PRs[tokenId][msg.sender];
         if (positive) {
@@ -191,6 +208,10 @@ contract Content is ERC1155, Ownable, IERC1155Receiver{
         }
         voteCredits[tokenId][msg.sender] -= (_numVotes ** 2); 
         emit Voted(msg.sender, _PRowner, _numVotes, positive, tokenId);
+    }
+
+    function voteToComplete(address _PRowner, uint tokenId) external onlyAuthor(tokenId) {
+
     }
 
     // TODO approvePR should be called by the ContentFactory on all contracts at the same time
@@ -268,6 +289,5 @@ contract Content is ERC1155, Ownable, IERC1155Receiver{
             PRexists[tokenId][PRauthor] = false;
         }
         delete PRauthors[tokenId];
-        delete topPRAuthors[tokenId];
     }
 }
