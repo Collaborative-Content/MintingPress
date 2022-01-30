301c278d930c70784ade171b1dfae137274034fe
mardet987 <marci.detwiller@gmail.com>
2022-01-30 07:13:25 -0600

    Name change

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ export default function Navigate() {
   return (
 <Navbar bg="dark" expand="lg" variant="dark">
   <Container>
-    <Navbar.Brand href="home">Collabory</Navbar.Brand>
+    <Navbar.Brand href="home">MintingPress</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
