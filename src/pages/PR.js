9d80422f3b347083ec79158bbd24dac85620e22a
rsundar <rohan.s.1987@gmail.com>
2022-01-30 18:15:25 +0530

    added a rudimentary PR page

--- src/pages/PR.js
+++ src/pages/PR.js
@@ -1,5 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
+import Navigate from '../components/NavBar';
+import StoryBox from '../components/StoryBox';
 
-export default function PR() {
-  return <div></div>;
+export default function PR({story}) {
+  const [pr, setPr] = useState([]);
+  const prRef = useState([]);
+
+  function handleSubmitPR() {
+    const price = prRef.current.value;
+    //sign and send the PR and handle any exceptions
+  }
+  
+  return (
+    <>
+      <Navigate>
+      </Navigate>
+      <Container>
+        <StoryBox story={story}>
+        </StoryBox>
+      </Container>
+      <label>Price for PR</label>
+      <input type="text" ref={prRef}/>
+      <button onClick={handleSubmitPR}>Sumbit Pull Request</button>
+    </>
+  );
 }
--- src/pages/Vote.js
+++ src/pages/Vote.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
 export default function Vote() {
-  return <div></div>;
+  return (
+    <></>
+  );
 }
