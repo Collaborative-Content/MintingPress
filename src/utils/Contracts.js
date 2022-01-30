4511739b4745660ea182b66bb1ac3d94b76da380
mardet987 <marci.detwiller@gmail.com>
2022-01-29 21:30:45 -0600

    mint function

--- src/utils/Contracts.js
+++ src/utils/Contracts.js
@@ -1,5 +1,5 @@
 import { ADMIN_ADDR, CONTENT_ADDR } from "../constants";
-import { getContract } from "./common";
+import { getContract, requestAccount } from "./common";
 import ContentArtifact from "../abis/Content.json"
 import AdminArtifact from "../abis/AdminProxy.json"
 
@@ -9,4 +9,16 @@ function getContentContract() {
 
 function getAdminContract() {
     return getContract(ADMIN_ADDR, AdminArtifact);
+}
+
+function mint(tokensymbol, supply, ownerStake, initialprice, story, value) {
+    encoder = new TextEncoder();
+    tempData = encoder.encode(story);
+    contract = getContentContract();
+    account = requestAccount();
+    overridesWithETH = {
+        value: value
+    };
+    await expect(contract.connect(account).mint(
+        tokensymbol, supply, ownerStake, initialprice, tempData, overridesWithETH))
 }
