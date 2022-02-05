4d90f7df8570149b54231d3f5668d3db1bee64d3
mardet987 <marci.detwiller@gmail.com>
2022-02-04 21:48:42 -0600

    additional fields on mint page

--- src/pages/Mint.js
+++ src/pages/Mint.js
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import StoryBox from "../components/StoryBox";
-import { Container, Button } from "react-bootstrap";
+import { Container, Button, Row, Form, Col } from "react-bootstrap";
 import { toast } from "react-toastify";
 
 import React from "react";
@@ -17,13 +17,42 @@ export default function Mint() {
       <Container>
         <div className="text-center">
           <img
-            style={{ height: "100px", margin: "20px auto", width: "200px" }}
-            src="https://media.istockphoto.com/vectors/printing-money-quantitative-easing-policy-by-countries-central-bank-vector-id1312056466?k=20&m=1312056466&s=612x612&w=0&h=V5Y9lrSoDDR5gWRO-kRuHZTuDzEPnrArvknnl1zEYQc="
+            style={{ height: "200px", margin: "20px auto", width: "200px" }}
+            src="https://cdn.discordapp.com/attachments/931173500289437817/937359950659858512/pp2.png"
           />
         </div>
 
         <StoryBox story={""} />
       </Container>
+      <Container> 
+      <Row className="mb-3">
+    <Form.Group as={Col} controlId="formGridTokenSymbol">
+      <Form.Label>Token Symbol</Form.Label>
+      <Form.Control />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridSupply">
+    <Form.Label>Supply</Form.Label>
+      <Form.Control />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridOwnerStake">
+      <Form.Label>Owner Stake</Form.Label>
+      <Form.Control />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridMinPRPrice">
+      <Form.Label>Min PR Price</Form.Label>
+      <Form.Control />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridETH">
+      <Form.Label>ETH Value</Form.Label>
+      <Form.Control />
+    </Form.Group>
+
+  </Row>
+  </Container>
       <Container>
         <Button
           variant="primary"
